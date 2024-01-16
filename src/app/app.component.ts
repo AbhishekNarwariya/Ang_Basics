9078e02545c08bb0da980da8a5e756f53f05824d
AbhishekNarwariya <narwariyaabhishek50@gmail.com>
2024-01-16 20:24:28 +0530

    adding reactivr form code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_basics';
+
+  myform:FormGroup
+
+
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.myform = this.fb.group({
+      username:[''],
+      password:['']
+    })
+   
+  }
+
+  onSubmit(){
+    console.log("submitted");
+    
+
+  }
+
+ 
+ 
 }
